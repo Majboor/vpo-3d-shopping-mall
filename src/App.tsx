19986d850d43ebeda88e3bab4383e8acb2b7eb82
Waleed Ajmal <82969990+waleedsworld@users.noreply.github.com>
2025-12-06 10:20:00 +0500

    Scaffold Vite + React + TypeScript app with Tailwind and shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import Scene2Test from "./pages/Scene2Test";
+import Scene3Test from "./pages/Scene3Test";
+import GalleryEditorial from "./pages/GalleryEditorial";
+import Fashion3DTest from "./pages/Fashion3DTest";
+import VPOBusiness from "./pages/VPOBusiness";
+import CaseStudies from "./pages/CaseStudies";
+import Blog from "./pages/Blog";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/scene2-test" element={<Scene2Test />} />
+          <Route path="/scene3-test" element={<Scene3Test />} />
+          <Route path="/gallery" element={<GalleryEditorial />} />
+          <Route path="/fashion-3d-test" element={<Fashion3DTest />} />
+          <Route path="/vpo-business" element={<VPOBusiness />} />
+          <Route path="/business" element={<VPOBusiness />} />
+          <Route path="/case-studies" element={<CaseStudies />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/journal" element={<Blog />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
